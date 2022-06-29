1840c856b257456c76a87f0d250df01d17dd8c9f
Adam Makuchowski <makuchowski.adam@gmail.com>
2022-06-29 22:13:41 +0200

    end front static structure

---
+++ questionsContent.js
@@ -0,0 +1,27 @@
+const questionsContent = [
+    {
+        quest:'What was the name of Ciri`s sword ?',
+        options:['Zirael','Falka','Imlerith','Ge`els'],
+        correct:0
+    },
+    {
+        quest:'Po kim Cahir odziedziczył swe koleje imiona (Mawr Dyffryn) ?',
+        options:['After my father and grandfather','After my mother and great-grandfather','After my father and great-grandfather','After my mother and grandfather'],
+        correct:1
+    },
+    {
+        quest:'Leo Bonhart, collected three witcher medallions in his life as a trophy. Which ones?',
+        options:['Three wolf medallions','The wolf, the cat and the vipers','The wolf and the cat','The wolf, the cat and the griffin'],
+        correct:3
+    },
+    {
+        quest:'What was the real name of the Bloody Baron ?',
+        options:['Folan Stenger','Filip Stenger','Fercart Stenger','Fergus Stenger'],
+        correct:0
+    },
+    {
+        quest:'What was the secret of Niellen, a hunter from the village of Konary ?',
+        options:['He was terminally ill','He was a werewolf','He was a vampire','He was a Doppler'],
+        correct:1
+    }
+]
