a5c7840c44e0cc3b36e3c2b97ab1e89bd51fcaa6
Adam Makuchowski <makuchowski.adam@gmail.com>
2022-07-01 20:17:37 +0200

    init randomness functionality set questions

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const questionNumber = document.querySelector('.question-number')
 const questionContent = document.querySelector('.question-content')
 const answerContainer = document.querySelector('.answer-container')
 
+const nextQuestionButton = document.querySelector('.next-question-button')
+
 let questionCounter = 0
 let currentQuestion
 let availableQuestions = []
+let avaiableAnswers = []
 
 const setAvailableQuestions = () => {
     for (const question of questionsContent) {
@@ -17,11 +20,33 @@ const getNewQuestion = () => {
 
     const randomQuestion = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
     currentQuestion = randomQuestion
-    questionContent.innerHTML = currentQuestion.quest
+    questionContent.innerHTML = currentQuestion.question
+
+    const indexToRemove = availableQuestions.indexOf(currentQuestion)
+    availableQuestions.splice(indexToRemove, 1)
+
+    for (const answer of currentQuestion.answers) {
+        const option = document.createElement('div')
+        option.innerHTML = answer
+        option.className = 'answer'
+        answerContainer.appendChild(option)
+    }
+
+    questionCounter++
+}
+
+const nextQuestion = () => {
+    if(questionCounter === questionsContent.length){
+        console.log('quiz over')
+    }
+    else {
+        getNewQuestion()
+    }
 }
 
 window.onload = function() {
-
     setAvailableQuestions()
     getNewQuestion()
 }
+
+nextQuestionButton.addEventListener('click', nextQuestion)
