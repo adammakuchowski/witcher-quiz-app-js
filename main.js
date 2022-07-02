96832871e74c58f4cbffb995699eba296867e0de
Adam Makuchowski <makuchowski.adam@gmail.com>
2022-07-02 18:36:43 +0200

    init answers animation

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const nextQuestionButton = document.querySelector('.next-question-button')
 let questionCounter = 0
 let currentQuestion
 let availableQuestions = []
-let avaiableAnswers = []
+let answerId = 0
 
 const setAvailableQuestions = () => {
     for (const question of questionsContent) {
@@ -25,14 +25,46 @@ const getNewQuestion = () => {
     const indexToRemove = availableQuestions.indexOf(currentQuestion)
     availableQuestions.splice(indexToRemove, 1)
 
+    let animationDelay = 0.1
+    answerContainer.innerHTML = ''
+    answerId = 0
+
     for (const answer of currentQuestion.answers) {
         const option = document.createElement('div')
         option.innerHTML = answer
+        option.id = answerId
         option.className = 'answer'
         answerContainer.appendChild(option)
+        option.style.animationDelay = `${animationDelay}s`
+        animationDelay += 0.1
+        answerId++
+
+        option.setAttribute('onclick', 'giveAnswer(this)')
     }
 
     questionCounter++
+}
+
+const giveAnswer = (answer) => {
+    if(answer.id == currentQuestion.correct){
+        answer.classList.add('correct')
+    }else{
+        answer.classList.add('wrong')
+
+        for(let i = 0; i < answerContainer.children.length; i++){
+            if(answerContainer.children[i].id == currentQuestion.correct){
+                answerContainer.children[i].classList.add('correct')
+            }
+        }
+    }
+
+    freezeAnswers()
+}
+
+const freezeAnswers = () => {
+    for(let i = 0; i < answerContainer.children.length; i++){
+        answerContainer.children[i].classList.add('freeze')
+    }
 }
 
 const nextQuestion = () => {
